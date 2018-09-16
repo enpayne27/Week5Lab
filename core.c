97519f1b8373bd0662144b9d6ecba8def91deff6
V-FEXrt <ash123125@yahoo.com>
2018-09-16 16:38:54 -0500

    Lab 5 Starting Files

---
+++ core.c
@@ -0,0 +1,27 @@
+//
+//  core.c
+//  Week 5 
+//
+//  Created by Ashley Coleman on 7/5/18.
+//  Copyright © 2018 Ashley Coleman. All rights reserved.
+//
+
+#include <stdio.h>
+
+#include "core.h"
+
+int swap(int * a, int * b) {
+    return FALSE;
+}
+
+int to_lower(char * s) {
+    return FALSE;
+}
+
+int inc_int_list(int * list, int length) {
+    return FALSE;
+}
+
+int core_main(int argc, const char * argv[]) {
+    return 0;
+}
